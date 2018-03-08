e04f341a94e60f4f0201222039b4a3668dc1ddd3
raulandres <andresgarciaraul@gmail.com>
2018-03-08 20:15:53 +0100

    test again

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_security_group" "default" {
-    count = var.aws_security_group["sg_count"]
+    count = "${var.aws_security_group.sg_count}"
 
     name = "terraform_security_group_${lookup(var.aws_security_group, concat("sg_", count.index, "_name"))}"
     description = "SG-TERRAFORM"
