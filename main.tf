7ebc0af4bb29eef26ec8d75d55bd561c10df9af5
raulandres <andresgarciaraul@gmail.com>
2018-03-08 19:00:51 +0100

    adding SG

--- main.tf
+++ main.tf
@@ -7,3 +7,15 @@ resource "aws_security_group" "default" {
 
     name = "terraform_security_group_${lookup(var.aws_security_group, concat("sg_", count.index, "_name"))}"
     description = "SG-TERRAFORM"
+
+    ingress {
+        from_port   = "${lookup(var.aws_security_group, concat("sg_", count.index, "_from_port"))}"
+        to_port     = "${lookup(var.aws_security_group, concat("sg_", count.index, "_to_port"))}"
+        protocol    = "${lookup(var.aws_security_group, concat("sg_", count.index, "_protocol"))}"
+        cidr_blocks = [ "0.0.0.0/0" ]
+    }
+
+    tags {
+        Name = "SG-TERRAFORM"
+    }
+}
--- terraform.tfvars
+++ terraform.tfvars
@@ -5,3 +5,15 @@ aws_key_path = ""
 
 aws_instance_user = ""
 aws_instance_type = ""
+
+aws_security_group.sg_count = "2"
+
+aws_security_group.sg_0_name = "ssh"
+aws_security_group.sg_0_from_port = "22"
+aws_security_group.sg_0_to_port = "22"
+aws_security_group.sg_0_protocol = "tcp"
+
+aws_security_group.sg_1_name = "http"
+aws_security_group.sg_1_from_port = "80"
+aws_security_group.sg_1_to_port = "80"
+aws_security_group.sg_1_protocol = "tcp"
