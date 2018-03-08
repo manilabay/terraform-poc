537978c7f7c69511ebc29e42f6d0121602ee02fe
raulandres <andresgarciaraul@gmail.com>
2018-03-08 18:57:25 +0100

    initial commit with main and terraform.tfvars

---
+++ main.tf
@@ -0,0 +1,9 @@
+provider "aws" {
+    region = "${var.aws_region}"
+}
+
+resource "aws_security_group" "default" {
+    count = "${var.aws_security_group.sg_count}"
+
+    name = "terraform_security_group_${lookup(var.aws_security_group, concat("sg_", count.index, "_name"))}"
+    description = "SG-TERRAFORM"
---
+++ terraform.tfvars
@@ -0,0 +1,7 @@
+aws_region = "eu-west-1"
+
+aws_key_name = ""
+aws_key_path = ""
+
+aws_instance_user = ""
+aws_instance_type = ""
